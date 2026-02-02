5cddd2e4c2e270730db7efc4a4222617e81c2649
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2026-02-01 20:23:22 -0800

    base cambiada temp para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://Adrinc.github.io',
-  base: '/cbl_mdf_demo_web',
+  base: '/',
   integrations: [react(), tailwind()],
   output: 'static',
 });
